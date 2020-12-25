8be4ece30067e5054554303daae54e641dd2afbb
trizzilla <trisangjigme@gmail.com>
2020-12-25 20:20:51 +0600

    added headers and few components

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+import "./header.styles.scss";
+
+const Header = () => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOPS
+      </Link>
+      <Link className="option" to="/contact">
+        CONTACT
+      </Link>
+      <Link className="option" to="/about">
+        About us
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
