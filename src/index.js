d4f104c8c12d507e22bc41c43fb346362c8ff8da
trizzilla <trisangjigme@gmail.com>
2020-12-30 21:13:18 +0600

    INITIALIZED REDUX

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+
+import store from "./redux/store";
+
+import App from "./App";
+import "./index.css";
 //import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 
   document.getElementById("root")
 );
